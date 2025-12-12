c4fbdda9cd184644228eb9e47d45284b94372582
Calpurniax <sofia.gracia@gmail.com>
2025-12-12 10:41:33 +0100

    añadir cosas al readme

--- Baraja.java
+++ Baraja.java
@@ -1,21 +1,46 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Baraja {
-    Carta[] baraja; 
-    
-    public void barajear(){
-        //he encontrado este método (Collections.shuffle) para mezclar arrays, pero se 
-        //usa con objetos lista y no con objetos array
-        //entonces es pasar el array a lista
-         List<Carta> barajaLista = Arrays.asList(baraja);
+    Carta[] baraja;
+    //Set<List<Carta>> baraja = new HashSet<>();
+
+    //el problema es que no ve la lista de objetos carta como una lista de objetos
+
+    public Baraja() {     
+        ArrayList<String> valores = new ArrayList<>();
+        Set<String> setValor = new HashSet<>(Arrays.asList("1", "2", "3", "4","5","6","7","8","9","10","J","Q","K"));
+        Set<Character> setPalo = new HashSet<>(Arrays.asList('D', 'C', 'P', 'T'));
+
+        //se puede cambiar esto a un array? para q luego se pueda poner = a baraja o como hacemos esto
+
+        Set<List<Object>> cartesianProduct = new HashSet<>();
+
+         for (String i: setValor) {
+            for (Character j: setPalo) {           
+                List<Object> tuple = Arrays.asList(i,j);
+                cartesianProduct.add(tuple);   
+         }
+        }
+        this.baraja=cartesianProduct;
+
+    }
+
+    public void barajear() {
+        // he encontrado este método (Collections.shuffle) para mezclar arrays, pero se
+        // usa con objetos lista y no con objetos array
+        // entonces es pasar el array a lista
+        List<Carta> barajaLista = Arrays.asList(baraja);
 
         // se mezcla
         Collections.shuffle(barajaLista);
 
         // Se pasa a array
         Carta[] barajaMezclada = barajaLista.toArray(new Carta[0]);
-        this.baraja=barajaMezclada;
+        this.baraja = barajaMezclada;
     }
 }
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 # Juego de BlackJack
 
 ## Distintos objetos identificados:
-1. Carta: es el un objeto con tres atributos: nombre, palo y representación gráfica. Crear dos Sets uno con nombres y otro palos, con el producto cartesiano se genera la baraja entera. Los valores se escriben en una función auxiliar. Hay problemas con el unicode y las terminales así que usaremos letras para los palos T (tréboles) P (picas) D (diamantes) C (corazones)
+1. Carta: es el un objeto con tres atributos: nombre, palo y representación gráfica. Crear dos Sets uno con nombres y otro palos, con el producto cartesiano se genera la baraja entera. Los valores se escriben en una función auxiliar. Hay problemas con el unicode y las terminales así que usaremos letras para los palos T (tréboles) P (picas) D (diamantes) C (corazones). Métodos para imprimir la carta, 
 1. Baraja: tiene 52 cartas, podemos crear la baraja siendo un array de las 52 cartas pero tenemos que conseguir que sean únicas. Incluye un método para mezclarlas.
-1. Mano: recibe cartas de la baraja, sus atributos son: número de cartas y sí son visibles o no (no sé si esto es propio de la carta o de la mano solo del Croupier). Incluye métodos para añadir o quitar una carta (cuando acaba el turno)
+1. Mano: recibe cartas de la baraja, sus atributos son: array de cartas, y sí son visibles o no (no sé si esto es propio de la carta o de la mano solo del Croupier). Incluye métodos para añadir o quitar una carta (cuando acaba el turno), calcula la puntuación del jugador y el crupier y calcula el valor del as si se pasa de 21.
 1. Persona que puede ser jugador o Croupier, la persona tiene mano y sumatorio de puntos
     1. Jugador: Atributo dinero y método para apostar
     1. Croupier: sus puntos están ocultos y una de las cartas también. Gestiona la baraja. 
