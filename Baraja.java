6aaee8195d5959402963c2c1ac0edfc553d855b5
Calpurniax <sofia.gracia@gmail.com>
2025-12-16 12:41:13 +0100

    cambiado la baraja a un array list para hacerlo más sencillo, cambiado sus métodos de nuevo

--- Baraja.java
+++ Baraja.java
@@ -4,21 +4,17 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Baraja {
-    //Carta[] baraja =  new Carta[52];   
-    
+public class Baraja {    
+
     ArrayList<Carta> baraja = new ArrayList<Carta>();
     
     public Baraja() {     
         Set<String> setValor = new HashSet<>(Arrays.asList("A", "2", "3", "4","5","6","7","8","9","10","J","Q","K"));
         Set<Character> setPalo = new HashSet<>(Arrays.asList('D', 'C', 'P', 'T'));
-        //int cont =0;
         for (String i: setValor) {
             for (Character j: setPalo) {   
                 Carta card =new Carta(i,j);
                 this.baraja.add(card);
-                // this.baraja[cont]=(card);
-                // cont++;
             }
         }
     }
@@ -39,7 +35,6 @@ public class Baraja {
          
         int valor = lastCard.getPuntos();
 
-       // int valor = Valores.getValor(lastCard);
        System.out.println("el valor de la última carta es "+ valor); 
        Crupier cr = new Crupier(baraja);       
        cr.pintarCarta();
